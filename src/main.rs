5ba01fbccc211019a917c2a4be02212d7841cf80
함윤식 <43927226+Ham3798@users.noreply.github.com>
2024-01-07 21:12:31 +0900

    init2

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,3 @@
-mod compiler;
-mod interpreter;
-mod parser;
-mod types;
-mod utils;
-
 fn main() {
-    println!("Language Interpreter/Compiler 시작");
-    // 여기에서 언어 인터프리터 또는 컴파일러 로직을 시작합니다.
-    // 예: 파일 읽기, 소스 코드 파싱, 실행 등
+    println!("hello world!");
 }
