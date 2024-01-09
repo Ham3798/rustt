8a40392cbcbf2128ffee839f7cca26b3cea5a2f4
함윤식 <43927226+Ham3798@users.noreply.github.com>
2024-01-09 15:30:14 +0900

    Modify: TokenKind 변경 rustc의 코드 긁어옴

--- rusttc_lexer/src/lib.rs
+++ rusttc_lexer/src/lib.rs
@@ -2,21 +2,154 @@ mod cursor;
 
 use cursor::Cursor;
 
-#[derive(Debug, PartialEq, Clone)]
+#[derive(Clone, Debug)]
 pub enum TokenKind {
+    // Multi-char tokens:
+    /// "// comment"
+    LineComment { doc_style: Option<DocStyle> },
+
+    /// `/* block comment */`
+    ///
+    /// Block comments can be recursive, so a sequence like `/* /* */`
+    /// will not be considered terminated and will result in a parsing error.
+    BlockComment { doc_style: Option<DocStyle>, terminated: bool },
+
+    /// Any whitespace character sequence.
+    Whitespace,
+
+    /// "ident" or "continue"
+    ///
+    /// At this step, keywords are also considered identifiers.
     Ident,
-    Number,
-    Plus,
+
+    /// Like the above, but containing invalid unicode codepoints.
+    InvalidIdent,
+
+    /// "r#ident"
+    RawIdent,
+
+    /// An unknown prefix, like `foo#`, `foo'`, `foo"`.
+    ///
+    /// Note that only the
+    /// prefix (`foo`) is included in the token, not the separator (which is
+    /// lexed as its own distinct token). In Rust 2021 and later, reserved
+    /// prefixes are reported as errors; in earlier editions, they result in a
+    /// (allowed by default) lint, and are treated as regular identifier
+    /// tokens.
+    UnknownPrefix,
+
+    /// Examples: `12u8`, `1.0e-40`, `b"123"`. Note that `_` is an invalid
+    /// suffix, but may be present here on string and float literals. Users of
+    /// this type will need to check for and reject that case.
+    ///
+    /// See [LiteralKind] for more details.
+    Literal { kind: LiteralKind, suffix_start: u32 },
+
+    /// "'a"
+    Lifetime { starts_with_number: bool },
+
+    // One-char tokens:
+    /// ";"
+    Semi,
+    /// ","
+    Comma,
+    /// "."
+    Dot,
+    /// "("
+    OpenParen,
+    /// ")"
+    CloseParen,
+    /// "{"
+    OpenBrace,
+    /// "}"
+    CloseBrace,
+    /// "["
+    OpenBracket,
+    /// "]"
+    CloseBracket,
+    /// "@"
+    At,
+    /// "#"
+    Pound,
+    /// "~"
+    Tilde,
+    /// "?"
+    Question,
+    /// ":"
+    Colon,
+    /// "$"
+    Dollar,
+    /// "="
+    Eq,
+    /// "!"
+    Bang,
+    /// "<"
+    Lt,
+    /// ">"
+    Gt,
+    /// "-"
     Minus,
+    /// "&"
+    And,
+    /// "|"
+    Or,
+    /// "+"
+    Plus,
+    /// "*"
     Star,
+    /// "/"
     Slash,
-    Equals,
-    Semicolon,
-    Unknown,
-    Eof,
+    /// "^"
+    Caret,
+    /// "%"
+    Percent,
 }
 
-#[derive(Debug, PartialEq, Clone)]
+#[derive(Clone, Debug)]
+pub enum LiteralKind {
+    /// "12_u8", "0o100", "0b120i99", "1f32".
+    Int { base: Base, empty_int: bool },
+    /// "12.34f32", "1e3", but not "1f32".
+    Float { base: Base, empty_exponent: bool },
+    /// "'a'", "'\\'", "'''", "';"
+    Char { terminated: bool },
+    /// "b'a'", "b'\\'", "b'''", "b';"
+    Byte { terminated: bool },
+    /// ""abc"", ""abc"
+    Str { terminated: bool },
+    // /// "b"abc"", "b"abc"
+    // ByteStr { terminated: bool },
+    // /// `c"abc"`, `c"abc`
+    // CStr { terminated: bool },
+    // /// "r"abc"", "r#"abc"#", "r####"ab"###"c"####", "r#"a". `None` indicates
+    // /// an invalid literal.
+    // RawStr { n_hashes: Option<u8> },
+    // /// "br"abc"", "br#"abc"#", "br####"ab"###"c"####", "br#"a". `None`
+    // /// indicates an invalid literal.
+    // RawByteStr { n_hashes: Option<u8> },
+    // /// `cr"abc"`, "cr#"abc"#", `cr#"a`. `None` indicates an invalid literal.
+    // RawCStr { n_hashes: Option<u8> },
+}
+
+#[derive(Clone, Debug)]
+pub enum Base {
+    /// Literal starts with "0b".
+    Binary = 2,
+    /// Literal starts with "0o".
+    Octal = 8,
+    /// Literal doesn't contain a prefix.
+    Decimal = 10,
+    /// Literal starts with "0x".
+    Hexadecimal = 16,
+}
+
+#[derive(Clone, Debug)]
+pub enum DocStyle {
+    Outer,
+    Inner,
+}
+
+#[derive(Clone, Debug)]
 pub struct Token {
     pub kind: TokenKind,
     pub text: String,
